605ff508f4dd8e86e248b73e415d34a539b5d455
sebastien78000 <60258199+sebastien78000@users.noreply.github.com>
2021-12-03 10:31:10 +0100

    Creation case

---
+++ ProjetDesignPatternSoftDev/Case.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDesignPatternSoftDev
+{
+    public class Case
+    {
+        string name;
+        int index;
+        string colour;
+
+        public Case(string name, int index, string colour)
+        {
+            this.name = name;
+            this.index = index;
+            this.colour = colour;
+        }
+    }
+}
